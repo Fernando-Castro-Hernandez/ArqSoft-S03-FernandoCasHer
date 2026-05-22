d0a241bf514f53ddc41277e81c4c1a413362d3c9
Fernando-Castro-Hernandez <jesus.castro@tecdesoftware.edu.mx>
2026-05-21 23:04:50 -0600

    Fix: Los modelos Items con valor de String se cambiaron para que siempre tengan valor

--- Catalogo.Domain/Models/Item.cs
+++ Catalogo.Domain/Models/Item.cs
@@ -3,11 +3,10 @@
     public class Item
     {
         public int Id { get; set; }
-        public string Nombre { get; set; }
+        public string Nombre { get; set; } = string.Empty;
         public int Elixir { get; set; }
         public string Calidad { get; set; } = string.Empty;
-        public string Tipo { get; set; }
+        public string Tipo { get; set; } = string.Empty;
         public int Arena { get; set; }
-
     }
 }
